21e7bf0be2960ed4bd5b1f4ef802d11626638f1b
hasanmehedi9034 <mehedi90344@gmail.com>
2023-04-01 21:15:07 +0600

    video slice ready

---
+++ src/features/videos/videosSlice.js
@@ -0,0 +1,38 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getVideos } from "./vidosAPI";
+
+const initialState = {
+  videos: [],
+  loading: false,
+  error: "",
+  isError: false,
+};
+
+export const fetchVideos = createAsyncThunk("videos/fetchVideos", async () => {
+  const videos = await getVideos();
+  return videos;
+});
+
+const videosSlice = createSlice({
+  name: "videos",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchVideos.pending, (state, action) => {
+        state.loading = true;
+        state.isError = false;
+      })
+      .addCase(fetchVideos.fulfilled, (state, action) => {
+        state.loading = false;
+        state.videos = action.payload;
+      })
+      .addCase(fetchVideos.rejected, (state, action) => {
+        state.loading = false;
+        state.isError = true;
+        state.error = action.error?.message;
+      });
+  },
+});
+
+export default videosSlice.reducer;
